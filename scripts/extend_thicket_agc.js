f98379f6c28a932c34ccae273236dd6fb5798b53
dugalh <dugalh@gmail.com>
2021-02-15 17:36:52 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -131,7 +131,7 @@ if (true)   //accuracy check
     scale: 1
   });
 
-var agc_diff = s2_calib_agc_plots.map(function(feature) {
+var agc_diff = calib_agc_plots.map(function(feature) {
   return feature.set({log_rn: ee.Number(feature.get('mean')).log10(), constant: 1});
 });
 
