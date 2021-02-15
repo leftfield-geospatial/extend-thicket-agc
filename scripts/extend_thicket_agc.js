3f8de02f3858d170dd6d8c10f670520e22b7f9c3
dugalh <dugalh@gmail.com>
2021-02-15 19:16:20 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -136,7 +136,7 @@ if (true)   //accuracy check
   // find residual sum of squares
   var agc_res_ss = s2_agc_calib_plots.map(function(feature) {
     return feature.set({agc_diff2: (ee.Number(feature.get('mean')).subtract(feature.get('AGC'))).pow(2)});
-  }).reduceColumns(ee.Reducer.sum(), ['agc_res_ss'])
+  }).reduceColumns(ee.Reducer.sum(), ['agc_res2'])
 
   // find sum of squares
   var agc_mean = ee.Number(s2_agc_calib_plots.reduceColumns(ee.Reducer.mean(), ['AGC']).get('mean'));
