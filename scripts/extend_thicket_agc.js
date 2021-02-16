97e21fbe38a8fc0bca86bbbc293325811d345201
dugalh <dugalh@gmail.com>
2021-02-16 15:06:18 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -126,7 +126,7 @@ var s2_agc = s2_rn.log10().multiply(calib_m.multiply(model_m)).add(calib_c.multi
 
 function accuracy_check(plots, agc_image, type)
 {
-  var agc_field = 'AGC'
+  var agc_field = 'AgcHa'
   var agc_plots = agc_image.reduceRegions({
     reducer: ee.Reducer.mean(),
     collection: plots,
