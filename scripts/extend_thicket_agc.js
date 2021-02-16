a9a442036c5c936454203e172dd82a9417f1bc9b
dugalh <dugalh@gmail.com>
2021-02-16 11:49:35 +0000

    Updated scripts/extend_thicket_agc.js (added 2 lines, deleted 2 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -59,8 +59,8 @@ function maskS2clouds(image)
   return image.updateMask(mask);
 }
 
-// var s2_images = ee.ImageCollection('COPERNICUS/S2_SR')    // not available for 2017
-var s2_images = ee.ImageCollection('COPERNICUS/S2')
+var s2_images = ee.ImageCollection('COPERNICUS/S2_SR')    // not available for 2017
+// var s2_images = ee.ImageCollection('COPERNICUS/S2')
 // var s2_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
                   .filterDate('2020-09-01', '2020-11-30')
                   // Pre-filter to get less cloudy granules.
