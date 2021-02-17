b1e4d68183a5fd178d2171e590fb4ba0f1db0c3c
dugalh <dugalh@gmail.com>
2021-02-17 16:45:29 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -39,7 +39,7 @@ var step_arid_and_valley_thicket = ee.FeatureCollection("users/dugalh/extend_thi
 // TODO: time series of ndvi/rn/agc in thicket
 // TODO: tool to find AGC in a user selected polygon
 // TODO: landsat and better s2 cloud mask - maybe not the shadow projection, 
-//    but the simple QA0 thing is not working great for S2, and SR data looks better, even if it doesn't 
+//     the simple QA0 thing is not working great for S2, and SR data looks better, even if it doesn't make better AGC?
 
 
 var model_m = ee.Number(-318.8304);
