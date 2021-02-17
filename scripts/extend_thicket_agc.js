220d3e30331c83a6ffee287ffaa17b1c9fb4161d
dugalh <dugalh@gmail.com>
2021-02-17 16:56:08 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -219,7 +219,7 @@ if (false)
   
 }
 
-var images = s2_sr_images;
+var images = l8_images;
 // print('num images: ', images.size());
 // print('images metadata: ', images);
 // print('image metadata: ', images.first());
