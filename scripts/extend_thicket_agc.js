5a2b57609714049304d1c45bc05b952a82a85935
dugalh <dugalh@gmail.com>
2021-02-17 16:18:59 +0000

    Updated scripts/extend_thicket_agc.js (added 9 lines, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -164,7 +164,15 @@ var s2_toa_images = ee.ImageCollection('COPERNICUS/S2')
                   .map(s2_cloud_mask)
                   .filterBounds(step_arid_and_valley_thicket);
 
-
+ var s2_sr_images = ee.ImageCollection('COPERNICUS/S2_SR')
+                  .filterDate('2019-09-01', '2019-11-30')
+                  // Pre-filter to get less cloudy granules.
+                  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10))  // CLOUDY_PIXEL_PERCENTAGE is in metadata (not a band)
+                  .filter(ee.Filter.lt('MEAN_SOLAR_ZENITH_ANGLE', 30))
+                  .filter(ee.Filter.lt('MEAN_INCIDENCE_ZENITH_ANGLE_B1', 30))
+                  .map(s2_cloud_mask)
+                  .filterBounds(step_arid_and_valley_thicket);
+ 
 if (false)
 {
   var s2_sr_images = ee.ImageCollection('COPERNICUS/S2_SR')
