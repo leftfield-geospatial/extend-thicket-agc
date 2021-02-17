ca50c4ebbed5cc7f31293a30076113853bfbfac7
dugalh <dugalh@gmail.com>
2021-02-17 18:00:13 +0000

    Updated scripts/extend_thicket_agc.js (added 2 lines, deleted 5 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -223,10 +223,6 @@ print('images metadata: ', images);
 print('SPACECRAFT_NAME: ', images.first().get('SPACECRAFT_NAME'));
 
 var image = images.mean();
-var masked_image = image.clip(step_arid_and_valley_thicket.geometry())
-Map.setOptions('TERRAIN');
-Map.centerObject(step_arid_and_valley_thicket);
-Map.addLayer(masked_image, {min: 0.0, max: 3000, bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'RGB');
 
 var rn_image = find_rn(image, 'L8');  //ee.String(images.first().get('SPACECRAFT_NAME'))
 print('rn_image: ', rn_image);
@@ -264,4 +260,5 @@ var agc_masked_image = agc_image.clip(step_arid_and_valley_thicket.geometry())
 Map.setOptions('TERRAIN');
 Map.centerObject(step_arid_and_valley_thicket);
 // Map.addLayer(masked_image.divide(10000), {min: 0.0, max: [0.3, 0.3, 0.3], bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'S2_SR');
+Map.addLayer(masked_image, {min: 0.0, max: 3000, bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'RGB');
 Map.addLayer(agc_masked_image, {min: 0, max: 40, palette: ['red', 'yellow', 'green'], opacity: 1.0}, 'AGC');
