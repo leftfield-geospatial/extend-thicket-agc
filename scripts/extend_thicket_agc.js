928b6239d09b7e8b61eef7da906450d09d30b4d5
dugalh <dugalh@gmail.com>
2021-02-17 16:59:43 +0000

    Updated scripts/extend_thicket_agc.js (added 2 lines, deleted 2 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -198,7 +198,7 @@ var s2_sr_images = ee.ImageCollection('COPERNICUS/S2_SR')
                   .map(s2_cloud_mask);
 
 
-var l8_images = ee.ImageCollection('LANDSAT/LC08/C01/T2_SR')
+var l8_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
                     .filterDate('2017-09-01', '2017-11-30')
                     .filterBounds(step_arid_and_valley_thicket)
                     // .map(landsat8_sr_cloud_mask);
@@ -213,7 +213,7 @@ if (false)
                   .filter(ee.Filter.lt('MEAN_INCIDENCE_ZENITH_ANGLE_B1', 30))
                   .map(s2_cloud_mask)
                   .filterBounds(step_arid_and_valley_thicket);
-  var l8_images = ee.ImageCollection('LANDSAT/LC08/C01/T2_TOA')
+  var l8_images = ee.ImageCollection('LANDSAT/LC08/C01/T2_SR')
                     .filterDate('2017-08-01', '2017-12-30')
                     .filterBounds(step_arid_and_valley_thicket);
   
