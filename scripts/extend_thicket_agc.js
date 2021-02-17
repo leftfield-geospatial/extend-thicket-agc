d6bbfd312a1506d8600d778ba4d3435616595dff
dugalh <dugalh@gmail.com>
2021-02-17 14:33:05 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -221,7 +221,7 @@ if (false)
 var agc_masked_image = agc_image.clip(step_arid_and_valley_thicket.geometry())
 Map.setOptions('TERRAIN');
 Map.centerObject(step_arid_and_valley_thicket);
-Map.addLayer(image.divide(10000), {min: 0.0, max: [0.3, 0.3, 0.3], bands: ['B4', 'B3', 'B2']}, 'RGB');
+Map.addLayer(image.divide(10000), {min: 0.0, max: [0.3, 0.3, 0.3], bands: ['B4', 'B3', 'B2'], opacity: 0.0}, 'RGB');
 Map.addLayer(agc_masked_image, {min: 0, max: 40, palette: ['red', 'yellow', 'green'], opacity: 0.8}, 'AGC');
 Map.addLayer(image, {min: 0, max: 40, palette: ['red', 'yellow', 'green'], opacity: 0.8}, 'AGC');
 // Map.addLayer(gef_calib_plots.draw({color: '660000', strokeWidth: 1}), {}, 'gef_calib_plots');
