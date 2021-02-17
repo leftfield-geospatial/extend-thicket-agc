fee4ea5e280b53a6bc899381041a6d44293d8373
dugalh <dugalh@gmail.com>
2021-02-17 12:22:45 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -181,7 +181,7 @@ print('image metadata: ', images.first());
 print('SPACECRAFT_NAME: ', images.first().get('SPACECRAFT_NAME'));
 
 var image = images.mean();
-var rn_image = find_rn(image, images.first().get('SPACECRAFT_NAME'));
+var rn_image = find_rn(image, ee.String(images.first().get('SPACECRAFT_NAME')));
 print('rn_image: ', rn_image);
 var agc_image = model_agc(rn_image);
 
