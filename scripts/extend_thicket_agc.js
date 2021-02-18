1876e1e2306722eb0eb1947c712c58e4db9911ae
dugalh <dugalh@gmail.com>
2021-02-18 13:53:30 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -188,7 +188,7 @@ function accuracy_check(agc_image, test_plots)
   // sum of squares
   var agc_ss = agc_plots.map(function(feature) {
     return feature.set({agc_off2: (ee.Number(feature.get('AgcHaPred')).subtract(agc_mean)).pow(2)});
-  }).reduceColumns(ee.Reducer.sum(), ['agc_off2'])
+  }).reduceColumns(ee.Reducer.sum(), ['agc_off2']).rename('sum','agc_ss')
   
   var agc_r2 = ee.Number(1).subtract(ee.Number(agc_res_ss.get('sum')).divide(ee.Number(agc_ss.get('sum'))))
   print('agc_r2: ', agc_r2)
