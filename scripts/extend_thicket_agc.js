9ec4aae4ba9b4741fd225ffd61d230c2ea25a1f4
dugalh <dugalh@gmail.com>
2021-02-18 13:53:01 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -169,7 +169,7 @@ function accuracy_check(agc_image, test_plots)
     reducer: ee.Reducer.mean(),
     collection: test_plots,
     scale: 1
-  }).rename(['mean'], ['AgcHaPred']);
+  }).rename(['mean'], [pred_agc_field]);
 
   // print('agc_plots: ', agc_plots)
 
