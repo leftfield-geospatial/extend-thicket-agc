e3676d959c577bf282d5a4b86255323d037a73cf
dugalh <dugalh@gmail.com>
2021-02-22 10:16:25 +0000

    Updated extend_thicket_agc/sentinel_cloud_masking.js (added 10 lines, deleted 8 lines).

--- extend_thicket_agc/sentinel_cloud_masking.js
+++ extend_thicket_agc/sentinel_cloud_masking.js
@@ -1,12 +1,14 @@
 export sentinelCloudMaskParams = { 
-AOI: ee.Geometry.Point(24, -33.5),
-START_DATE: '2017-10-01',
-END_DATE: '2017-11-01',
-CLOUD_FILTER: 60,
-CLD_PRB_THRESH: 25,
-NIR_DRK_THRESH: 0.15,
-CLD_PRJ_DIST: 4,
-BUFFER: 100,}
+    AOI: ee.Geometry.Point(24, -33.5),
+    START_DATE: '2017-10-01',
+    END_DATE: '2017-11-01',
+    CLOUD_FILTER: 60,
+    CLD_PRB_THRESH: 25,
+    NIR_DRK_THRESH: 0.15,
+    CLD_PRJ_DIST: 4,
+    BUFFER: 100
+}
+
 export function draw(ctx, length, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, length, length);
