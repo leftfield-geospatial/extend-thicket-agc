8fec33d2d916bc7098d5fad31c65ec2ea71e0b5d
dugalh <dugalh@gmail.com>
2021-02-22 10:31:16 +0000

    Updated scripts/extend_thicket_agc.js (added 2 lines, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -51,6 +51,7 @@ var step_arid_and_valley_thicket = ee.FeatureCollection("users/dugalh/extend_thi
 // S2 TOA has visible clouds with s2_cloud_mask and prob needs 'COPERNICUS/S2_CLOUD_PROBABILITY' 
 // S2 SR is is noticeably less hazy than TOA, but only available from 2019... why is that?
 // S2 TOA geom accuracy is much better than L8 T1
+var composites = require('users/dugalh/extend_thicket_agc/extend_thicket_agc:sentinel_cloud_masking');
 
 import {s2CloudMaskParams, get_s2_sr_cld_col} from '../extend_thicket_agc/sentinel_cloud_masking.js'
 
