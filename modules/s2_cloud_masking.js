f0bc5169646ad003aea5b6c5a29a802d09b9e196
dugalh <dugalh@gmail.com>
2021-02-22 10:52:17 +0000

    Updated modules/s2_cloud_masking.js (added 7 lines, deleted 5 lines).

--- modules/s2_cloud_masking.js
+++ modules/s2_cloud_masking.js
@@ -47,15 +47,17 @@ exports.get_s2_sr_cld_col = function(aoi, start_date, end_date)
     }));
 }
 
-def add_cloud_bands(img):
-    # Get s2cloudless image, subset the probability band.
-    cld_prb = ee.Image(img.get('s2cloudless')).select('probability')
+exports.add_cloud_bands(img)
+{
+    // Get s2cloudless image, subset the probability band.
+    var cld_prb = ee.Image(img.get('s2cloudless')).select('probability');
 
-    # Condition s2cloudless by the probability threshold value.
+    // # Condition s2cloudless by the probability threshold value.
     is_cloud = cld_prb.gt(CLD_PRB_THRESH).rename('clouds')
 
-    # Add the cloud probability layer and cloud mask as image bands.
+    // # Add the cloud probability layer and cloud mask as image bands.
     return img.addBands(ee.Image([cld_prb, is_cloud]))
+}
     
 def add_shadow_bands(img):
     # Identify water pixels from the SCL band.
