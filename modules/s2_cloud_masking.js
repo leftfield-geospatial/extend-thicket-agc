57d6ec761c4083a98fc565c12818b8de9e02bc88
dugalh <dugalh@gmail.com>
2021-02-22 16:09:25 +0000

    Updated modules/s2_cloud_masking.js (added 1 line, deleted 1 line).

--- modules/s2_cloud_masking.js
+++ modules/s2_cloud_masking.js
@@ -136,7 +136,7 @@ function add_cld_only_mask(img)
   // Remove small cloud-shadow patches and dilate remaining pixels by BUFFER input.
   // 20 m scale is for speed, and assumes clouds don't require 10 m precision.
   var scale = 1;
-  is_cld_shdw = (img_cloud.select('clouds').focal_min(2).focal_max(s2CloudMaskParams.BUFFER*2/20)
+  is_cld_shdw = (img_cloud.select('clouds').focal_min(2).focal_max(s2CloudMaskParams.BUFFER*2/scale)
       // .reproject({'crs': img.select([0]).projection(), 'scale': 20})
       .rename('cloudmask'));
   
