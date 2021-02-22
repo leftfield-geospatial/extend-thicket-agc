e7cca8d8f320e3b831e21e2478b0c3141799ec01
dugalh <dugalh@gmail.com>
2021-02-22 16:24:04 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -229,7 +229,7 @@ else if (false)
 
 else if (false)
   var l8_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR') //ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')  
-                      .filterDate('2017-09-01', '2017-11-30')
+                      .filterDate('2017-10-01', '2017-10-30')
                       .filterBounds(step_arid_and_valley_thicket)
                       .map(landsat8_sr_cloud_mask);
 
