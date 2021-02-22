91249768b58330d60f5cbd0dec2728c66764cd80
dugalh <dugalh@gmail.com>
2021-02-22 16:03:55 +0000

    Updated modules/s2_cloud_masking.js (added 3 lines, deleted 2 lines).

--- modules/s2_cloud_masking.js
+++ modules/s2_cloud_masking.js
@@ -106,10 +106,11 @@ function add_cld_shdw_mask(img)
   var img_cloud = add_cloud_bands(img);
   
   // Add cloud shadow component bands.
-  var img_cloud_shadow = add_shadow_bands(img_cloud);
+  // var img_cloud_shadow = add_shadow_bands(img_cloud);
   
   // Combine cloud and shadow mask, set cloud and shadow as value 1, else 0.
-  var is_cld_shdw = img_cloud_shadow.select('clouds').add(img_cloud_shadow.select('shadows')).gt(0);
+  // var is_cld_shdw = img_cloud_shadow.select('clouds').add(img_cloud_shadow.select('shadows')).gt(0);
+  var is_cld_shdw = img_cloud.select('clouds').gt(0);
   
   // Remove small cloud-shadow patches and dilate remaining pixels by BUFFER input.
   // 20 m scale is for speed, and assumes clouds don't require 10 m precision.
