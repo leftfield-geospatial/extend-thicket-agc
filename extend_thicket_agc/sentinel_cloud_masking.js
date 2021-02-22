7fd1c735604838b126dcbad47b117997f94053e4
dugalh <dugalh@gmail.com>
2021-02-22 10:17:59 +0000

    Updated extend_thicket_agc/sentinel_cloud_masking.js (added 24 lines).

--- extend_thicket_agc/sentinel_cloud_masking.js
+++ extend_thicket_agc/sentinel_cloud_masking.js
@@ -9,6 +9,30 @@ export sentinelCloudMaskParams = {
     BUFFER: 100
 }
 
+export function get_s2_sr_cld_col(aoi, start_date, end_date)
+{
+    # Import and filter S2 SR.
+    s2_sr_col = (ee.ImageCollection('COPERNICUS/S2')
+        .filterBounds(aoi)
+        .filterDate(start_date, end_date)
+        .filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', CLOUD_FILTER)))
+
+    # Import and filter s2cloudless.
+    s2_cloudless_col = (ee.ImageCollection('COPERNICUS/S2_CLOUD_PROBABILITY')
+        .filterBounds(aoi)
+        .filterDate(start_date, end_date))
+
+    # Join the filtered s2cloudless collection to the SR collection by the 'system:index' property.
+    return ee.ImageCollection(ee.Join.saveFirst('s2cloudless').apply(**{
+        'primary': s2_sr_col,
+        'secondary': s2_cloudless_col,
+        'condition': ee.Filter.equals(**{
+            'leftField': 'system:index',
+            'rightField': 'system:index'
+        })
+    }))
+}
+
 export function draw(ctx, length, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, length, length);
