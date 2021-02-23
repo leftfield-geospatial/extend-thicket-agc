eed3649cdc743629c43f62bf13f788fa958de136
dugalh <dugalh@gmail.com>
2021-02-23 16:16:15 +0000

    Updated scripts/extend_thicket_agc.js (added 3 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -248,10 +248,13 @@ else if (false)
                     .map(s2_cloud_mask);
 
 else if (true)
+{
   var l8_sr_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR') //ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')  
                       .filterDate('2017-09-01', '2017-12-30')
                       .filterBounds(step_arid_and_valley_thicket)
                       .map(landsat8_sr_cloud_mask);
+  gef_calib_plots = gef_calib_plots.map(function(plot){return plot})
+}                     
 else if (false)
   var l8_toa_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA') //ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')  
                       .filterDate('2017-11-01', '2017-12-30')
