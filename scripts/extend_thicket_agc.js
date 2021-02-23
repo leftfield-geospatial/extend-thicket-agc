a046aa1376814c5146612fa408cc3edf55c71286
dugalh <dugalh@gmail.com>
2021-02-23 10:50:49 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -73,7 +73,7 @@ function s2_cloud_mask(image)
   var bitMask = (1 << 11) | (1 << 10);
 
   // Both flags should be set to zero, indicating clear conditions.
-  var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(qa.bitwiseAnd(cirrusBitMask).eq(0)).focal_max(5);
+  var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(qa.bitwiseAnd(cirrusBitMask).eq(0));
 
   // return image.updateMask(qa.bitwiseAnd(bitMask).eq(0));
   return image.updateMask(mask);
