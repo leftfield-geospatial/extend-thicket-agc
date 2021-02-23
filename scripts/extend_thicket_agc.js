199cf05ba081d2271d0b3976c4ef93ef6aad060f
dugalh <dugalh@gmail.com>
2021-02-23 10:51:07 +0000

    Updated scripts/extend_thicket_agc.js (added 5 lines, deleted 5 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -68,15 +68,15 @@ function s2_cloud_mask(image)
   var qa = image.select('QA60');
 
   // Bits 10 and 11 are clouds and cirrus, respectively.
-  var cloudBitMask = 1 << 10;
-  var cirrusBitMask = 1 << 11;
+  // var cloudBitMask = 1 << 10;
+  // var cirrusBitMask = 1 << 11;
   var bitMask = (1 << 11) | (1 << 10);
 
   // Both flags should be set to zero, indicating clear conditions.
-  var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(qa.bitwiseAnd(cirrusBitMask).eq(0));
+  // var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(qa.bitwiseAnd(cirrusBitMask).eq(0));
 
-  // return image.updateMask(qa.bitwiseAnd(bitMask).eq(0));
-  return image.updateMask(mask);
+  return image.updateMask(qa.bitwiseAnd(bitMask).eq(0));
+  // return image.updateMask(mask);
 }
 
 function landsat_simple_cloud_mask(image)
