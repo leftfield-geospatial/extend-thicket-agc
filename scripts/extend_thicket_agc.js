99780edf5a0444b36f9c4e2a68f9cc75318c2284
dugalh <dugalh@gmail.com>
2021-02-23 11:39:39 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -264,7 +264,7 @@ else if (false)
 // convert AgcHa from kg to tons
 gef_sampling_plots = gef_sampling_plots.map(function(feature){return feature.set({AgcHa: ee.Number(feature.get('AgcHa')).divide(1000)})});
 
-var images = l8_images;
+var images = l8_toa_images;
 print('num images: ', images.size());
 print('images metadata: ', images);
 // print('image metadata: ', images.first());
