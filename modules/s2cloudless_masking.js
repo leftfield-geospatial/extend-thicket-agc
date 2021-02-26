1d7356d29b8ec74c2c86887b15153c76028ab7af
dugalh <dugalh@gmail.com>
2021-02-26 09:37:09 +0000

    Updated modules/s2cloudless_masking.js (deleted 7 lines).

--- modules/s2cloudless_masking.js
+++ modules/s2cloudless_masking.js
@@ -15,13 +15,6 @@
 // From https://developers.google.com/earth-engine/tutorials/community/sentinel-2-s2cloudless
 // Adapted and modified by Dugal Harris (dugalh@gmailcom) 
 
-// Straighforward cloud mask using QA60 band cloud bits
-function simple_cloud_mask(image) 
-{
-  var qa = image.select('QA60');
-  var bitMask = (1 << 11) | (1 << 10);
-  return qa.updateMask(qa.bitwiseAnd(bitMask).eq(0)); 
-}
 
 // Parameters for cloud and shadow masking with "s2cloudless" (cloud probability images)
 var s2_cloud_mask_params = { 
