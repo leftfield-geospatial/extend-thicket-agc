d43791a3d31a012dfa7f180c1bbf70acfdbafa26
dugalh <dugalh@gmail.com>
2021-02-26 09:49:04 +0000

    Updated modules/cloud_masking.js (added 1 line).

--- modules/cloud_masking.js
+++ modules/cloud_masking.js
@@ -26,6 +26,7 @@ function landsat8_sr_cloud_mask(image)
   return image.updateMask(qa.bitwiseAnd(mask_bit).eq(0));
 }
 
+// Cloud and shadow mask L8 TOA data with "pixel_qa" band
 function landsat8_toa_cloud_mask(image) 
 {
   // Bits 3 and 5 are cloud shadow and cloud, respectively.
