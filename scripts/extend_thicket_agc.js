d4682128080c777c9f59edeb063b395564e110c5
dugalh <dugalh@gmail.com>
2021-02-26 12:49:52 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -119,7 +119,7 @@ function model_agc(rn_image, train_plots)
   
   var calib_coeff = ee.Array(calib_res.get('coefficients')).toList();
   // print(calib_model.get('coefficients'))
-  //TODO - should these be made server side?
+  //should these be made server side?
   var calib_model = {m: ee.Number(ee.List(calib_coeff.get(0)).get(0)), c: ee.Number(ee.List(calib_coeff.get(1)).get(0))};
   var agc_ee_model = {m: calib_model.m.multiply(agc_model.m), c: calib_model.c.multiply(agc_model.m).add(agc_model.c)};
   
