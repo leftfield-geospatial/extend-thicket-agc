c9628552d534ec85bef5a5e7594dac896774d7b5
dugalh <dugalh@gmail.com>
2021-02-26 14:10:04 +0000

    Updated scripts/extend_thicket_agc.js (added 2 lines, deleted 2 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -314,7 +314,6 @@ var legend_title = ui.Label({
 
 // Add the legendPanel to the map.
 var legend_panel = ui.Panel([legend_title, color_bar, legend_labels]);
-Map.add(legend_panel);
 
 var agc_masked_image = agc_image.clip(step_arid_and_valley_thicket.geometry())
 var masked_image = image.clip(step_arid_and_valley_thicket.geometry())
@@ -324,3 +323,4 @@ Map.centerObject(step_arid_and_valley_thicket);
 // Map.addLayer(masked_image.divide(10000), {min: 0.0, max: [0.3, 0.3, 0.3], bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'S2_SR');
 // Map.addLayer(masked_image, {min: 0.0, max: 3000, bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'RGB', false);
 Map.addLayer(agc_masked_image, vis, 'AGC');
+Map.add(legend_panel);
