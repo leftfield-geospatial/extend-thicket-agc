9a852a555e3bd4e0b7a90893e6545a8f573072c5
dugalh <dugalh@gmail.com>
2021-02-26 09:45:06 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 38 lines).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -70,44 +70,7 @@ var agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)};
 // var agc_model = {m: ee.Number(-245.6729), c: ee.Number(11.5778)};  // pan = (R + G + B + NIR1)
 
 
-function landsat_simple_cloud_mask(image)
-{
-  var scored = ee.Algorithms.Landsat.simpleCloudScore(image);
-  
-  // Create a mask from the cloud score and combine it with the image mask.
-  var mask = scored.select(['cloud']).lte(5);
-  
-  // Apply the mask to the image and display the result.
-  return image.updateMask(mask);
-}
 
-function landsat8_sr_cloud_mask(image) 
-{
-  // Bits 3 and 5 are cloud shadow and cloud, respectively.
-  // var could_shadow_bit = (1 << 3);
-  // var cloud_bit = (1 << 5);
-  var mask_bit = (1 << 5) | (1 << 3);
-  // Get the pixel QA band.
-  var qa = image.select('pixel_qa');
-  // Both flags should be set to zero, indicating clear conditions.
-  // var mask = qa.bitwiseAnd(could_shadow_bit).eq(0)
-  //               .and(qa.bitwiseAnd(cloud_bit).eq(0));
-  // return image.updateMask(mask);
-  return image.updateMask(qa.bitwiseAnd(mask_bit).eq(0));
-}
-
-function landsat8_toa_cloud_mask(image) 
-{
-  // Bits 3 and 5 are cloud shadow and cloud, respectively.
-  // var could_shadow_bit = (1 << 3);
-  // var cloud_bit = (1 << 4);
-  var bit_mask = (1 << 4) | (1 << 8);    // cloud bit and upper bit of shadow confidence
-  // Get the pixel QA band.
-  var qa = image.select('BQA');
-  // Both flags should be set to zero, indicating clear conditions.
-  var mask = qa.bitwiseAnd(bit_mask).eq(0);
-  return image.updateMask(mask);
-}
 
 function find_rn(image) 
 {
