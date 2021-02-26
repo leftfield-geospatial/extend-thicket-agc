8a46bb61caf596af7693cf75127157d700fa6146
dugalh <dugalh@gmail.com>
2021-02-26 09:36:49 +0000

    Updated modules/s2cloudless_masking.js (added 1 line, deleted 1 line).

--- modules/s2cloudless_masking.js
+++ modules/s2cloudless_masking.js
@@ -12,7 +12,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.*/
 
-// Adapted by Dugal Harris (dugalh@gmailcom) from https://developers.google.com/earth-engine/tutorials/community/sentinel-2-s2cloudless
+// Adapted and modified by Dugal Harris (dugalh@gmailcom) from https://developers.google.com/earth-engine/tutorials/community/sentinel-2-s2cloudless
 
 // Straighforward cloud mask using QA60 band cloud bits
 function simple_cloud_mask(image) 
