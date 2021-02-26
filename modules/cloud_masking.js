4b50753abe75137c945ab5cc589f721758b2c100
dugalh <dugalh@gmail.com>
2021-02-26 09:45:17 +0000

    Updated modules/cloud_masking.js (added 38 lines).

--- modules/cloud_masking.js
+++ modules/cloud_masking.js
@@ -9,3 +9,41 @@ function s2_simple_cloud_mask(image)
 }
 exports.s2_simple_cloud_mask = s2_simple_cloud_mask;
 
+function landsat_simple_cloud_mask(image)
+{
+  var scored = ee.Algorithms.Landsat.simpleCloudScore(image);
+  
+  // Create a mask from the cloud score and combine it with the image mask.
+  var mask = scored.select(['cloud']).lte(5);
+  
+  // Apply the mask to the image and display the result.
+  return image.updateMask(mask);
+}
+
+function landsat8_sr_cloud_mask(image) 
+{
+  // Bits 3 and 5 are cloud shadow and cloud, respectively.
+  // var could_shadow_bit = (1 << 3);
+  // var cloud_bit = (1 << 5);
+  var mask_bit = (1 << 5) | (1 << 3);
+  // Get the pixel QA band.
+  var qa = image.select('pixel_qa');
+  // Both flags should be set to zero, indicating clear conditions.
+  // var mask = qa.bitwiseAnd(could_shadow_bit).eq(0)
+  //               .and(qa.bitwiseAnd(cloud_bit).eq(0));
+  // return image.updateMask(mask);
+  return image.updateMask(qa.bitwiseAnd(mask_bit).eq(0));
+}
+
+function landsat8_toa_cloud_mask(image) 
+{
+  // Bits 3 and 5 are cloud shadow and cloud, respectively.
+  // var could_shadow_bit = (1 << 3);
+  // var cloud_bit = (1 << 4);
+  var bit_mask = (1 << 4) | (1 << 8);    // cloud bit and upper bit of shadow confidence
+  // Get the pixel QA band.
+  var qa = image.select('BQA');
+  // Both flags should be set to zero, indicating clear conditions.
+  var mask = qa.bitwiseAnd(bit_mask).eq(0);
+  return image.updateMask(mask);
+}
