4447751d1152df448340531da851a442bcf1920c
dugalh <dugalh@gmail.com>
2021-02-26 09:48:49 +0000

    Updated modules/cloud_masking.js (deleted 7 lines).

--- modules/cloud_masking.js
+++ modules/cloud_masking.js
@@ -21,15 +21,8 @@ function landsat_simple_cloud_mask(image, thresh=5)
 function landsat8_sr_cloud_mask(image) 
 {
   // Bits 3 and 5 are cloud shadow and cloud, respectively.
-  // var could_shadow_bit = (1 << 3);
-  // var cloud_bit = (1 << 5);
   var mask_bit = (1 << 5) | (1 << 3);
-  // Get the pixel QA band.
   var qa = image.select('pixel_qa');
-  // Both flags should be set to zero, indicating clear conditions.
-  // var mask = qa.bitwiseAnd(could_shadow_bit).eq(0)
-  //               .and(qa.bitwiseAnd(cloud_bit).eq(0));
-  // return image.updateMask(mask);
   return image.updateMask(qa.bitwiseAnd(mask_bit).eq(0));
 }
 
