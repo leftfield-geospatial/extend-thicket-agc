465f32e37c99806032d84dd067aa1d46a29795ec
dugalh <dugalh@gmail.com>
2021-03-01 13:22:46 +0000

    Updated scripts/extend_thicket_agc.js (added 1 line, deleted 1 line).

--- scripts/extend_thicket_agc.js
+++ scripts/extend_thicket_agc.js
@@ -346,7 +346,7 @@ var slider_panel = ui.Panel({
 });
 // Add the legendPanel to the map.
 var legend_panel = ui.Panel({widgets: [legend_title, slider_panel, color_bar, legend_labels, 
-                                        ui.Label('Docs').setUrl('https://github.com/dugalh/extend_thicket_agc')],   
+                                        ui.Label('More information').setUrl('https://github.com/dugalh/extend_thicket_agc')],   
                             style: {
                                 position: 'bottom-left',
                                 padding: '4px 8px'
