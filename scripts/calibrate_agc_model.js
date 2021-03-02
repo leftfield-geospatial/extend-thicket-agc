7a392e8d51287d69eee143dcbcd647f3a70d3a2e
dugalh <dugalh@gmail.com>
2021-03-02 09:24:43 +0000

    Updated scripts/calibrate_agc_model.js (added 2 lines, deleted 2 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -4,8 +4,8 @@ var step_arid_and_valley_thicket = ee.FeatureCollection("users/dugalh/extend_thi
 // Calibrate the GEF AGC model to Landsat / Sentinel imagery and evaluate accuracy 
 
 var cloud_masking = require('users/dugalh/extend_thicket_agc:modules/cloud_masking.js');
-var thicket_boundary = step_arid_and_valley_thicket;
-var gef_agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)};
+var thicket_boundary = step_arid_and_valley_thicket;  // STEP derived thicket boundaries
+var gef_agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)}; // the univariate log(mean(R/pan)) WV3 model
 
 // Find Sentinel or Landsat normalised red image
 function find_rn(image) {
