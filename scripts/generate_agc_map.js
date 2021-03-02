656aac0b8b4d81a7aa54783486d938c02da8f03b
dugalh <dugalh@gmail.com>
2021-03-02 12:09:12 +0000

    Updated scripts/generate_agc_map.js (added 2 lines, deleted 7 lines).

--- scripts/generate_agc_map.js
+++ scripts/generate_agc_map.js
@@ -44,13 +44,8 @@ var vis = {min: 0, max: 50, palette: 'red,yellow,green', opacity: 1.0}
 
 Map.setOptions('HYBRID');
 Map.centerObject(thicket_boundary);
-// Map.addLayer(masked_image.divide(10000), {min: 0.0, max: [0.3, 0.3, 0.3], bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'S2_SR');
-// Map.addLayer(masked_image, {min: 0.0, max: 3000, bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'RGB', false);
-Map.addLayer(agc_masked_image, vis, 'AGC');
+Map.addLayer(agc_image, vis, 'AGC');
 Map.layers().get(0).setOpacity(.55)
-/*
- * Legend setup
- */
 
 // Creates a color bar thumbnail image for use in legend from the given color
 // palette.
