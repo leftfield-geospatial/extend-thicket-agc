bea9b5a22054b6199a34ad76ef064169a006ba82
dugalh <dugalh@gmail.com>
2021-03-02 10:05:00 +0000

    Updated scripts/calibrate_agc_model.js (added 5 lines, deleted 1 line).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -10,7 +10,11 @@ var thicket_boundary = step_arid_and_valley_thicket;  // STEP derived thicket bo
 var gef_agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)}; // the univariate log(mean(R/pan)) WV3 model
 
 // convert kg to tonnes
-gef_sampling_plots = gef_sampling_plots.map(function(feature){return feature.gee_log_rn({AgcHa: ee.Number(feature.get('AgcHa')).divide(1000)})});
+gef_sampling_plots = gef_sampling_plots.map(function(feature){
+  return feature.gee_log_rn({
+    AgcHa: ee.Number(feature.get('AgcHa')).divide(1000)
+  })
+});
 
 // Find R/pan image feature
 function find_rn(image) {
