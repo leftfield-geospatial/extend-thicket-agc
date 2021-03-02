8846e83c335de97f0a0669fd2f7cf6ad6fffc993
dugalh <dugalh@gmail.com>
2021-03-02 10:34:39 +0000

    Updated scripts/calibrate_agc_model.js (added 2 lines, deleted 1 line).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -150,5 +150,6 @@ var agc_ptile = agc_dict.image.reduceRegion({
 });
 print('2-98% EE AGC: ', agc_ptile);
 
-var ee_agc_model = ee.Dictionary(agc_dict.model)
+var ee_agc_model = ee.Dictionary(agc_dict.model);
+print(ee_agc_model) 
 Export.table.toAsset(ee_agc_model, 'ee_agc_model', 'extend_thicket_agc/ee_agc_model');
