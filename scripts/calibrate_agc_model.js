f5651a826643251d5983bd491337b6992502c4c7
dugalh <dugalh@gmail.com>
2021-03-02 10:51:25 +0000

    Updated scripts/calibrate_agc_model.js (added 2 lines, deleted 2 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -151,8 +151,8 @@ var agc_ptile = agc_dict.image.reduceRegion({
 print('2-98% EE AGC: ', agc_ptile);
 
 // export model and AGC image for use in other scripts
-var feat = ee.Feature(null, agc_dict.model);
+var model_feat = ee.Feature(null, agc_dict.model);
 print(ee_agc_model_feat);
-var ee_agc_model_coll = ee.FeatureCollection([ee_agc_model_feat]);
+var ee_agc_model_coll = ee.FeatureCollection([feat]);
 print(ee_agc_model_coll);
 Export.table.toAsset(ee_agc_model_coll, 'ee_agc_model', 'extend_thicket_agc/ee_agc_model');
