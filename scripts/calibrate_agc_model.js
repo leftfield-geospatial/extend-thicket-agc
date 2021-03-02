89675ece06ee241014f3625aa77cfac7fef693d3
dugalh <dugalh@gmail.com>
2021-03-02 10:44:34 +0000

    Updated scripts/calibrate_agc_model.js (added 1 line, deleted 1 line).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -152,6 +152,6 @@ print('2-98% EE AGC: ', agc_ptile);
 
 var ee_agc_model = ee.Dictionary(agc_dict.model);
 print(ee_agc_model);
-var ee_agc_model_ = ee.FeatureCollection(ee.List(ee.Dictionary(agc_dict.model)));
+var ee_agc_model_ = ee.FeatureCollection(ee.List([ee.Dictionary(agc_dict.model)]));
 print(ee_agc_model_);
 Export.table.toAsset(ee_agc_model_, 'ee_agc_model', 'extend_thicket_agc/ee_agc_model');
