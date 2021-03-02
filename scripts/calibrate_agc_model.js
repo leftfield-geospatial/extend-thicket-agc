6dd9f45a81f96b618dabc72492f39c396cc9193f
dugalh <dugalh@gmail.com>
2021-03-02 10:04:24 +0000

    Updated scripts/calibrate_agc_model.js (added 2 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -9,6 +9,8 @@ var cloud_masking = require('users/dugalh/extend_thicket_agc:modules/cloud_maski
 var thicket_boundary = step_arid_and_valley_thicket;  // STEP derived thicket boundaries
 var gef_agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)}; // the univariate log(mean(R/pan)) WV3 model
 
+gef_sampling_plots = gef_sampling_plots.map(function(feature){return feature.gee_log_rn({AgcHa: ee.Number(feature.get('AgcHa')).divide(1000)})});
+
 // Find R/pan image feature
 function find_rn(image) {
   var rn_image = image.expression('(R / (R + G + B + RE))', 
