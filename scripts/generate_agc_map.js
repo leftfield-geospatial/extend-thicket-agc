7c6ae17cba2cbc89965a843c2e47b08e237eea72
dugalh <dugalh@gmail.com>
2021-03-02 12:09:01 +0000

    Updated scripts/generate_agc_map.js (added 79 lines, deleted 3 lines).

--- scripts/generate_agc_map.js
+++ scripts/generate_agc_map.js
@@ -38,7 +38,83 @@ var rn_image = find_rn(image);
 
 // find the EE AGC image
 var agc_image = rn_image.log10().multiply(-156.1047).add(-80.1402);
+// var agc_masked_image = agc_image.clip(thicket_boundary.geometry())
 
+var vis = {min: 0, max: 50, palette: 'red,yellow,green', opacity: 1.0}
 
+Map.setOptions('HYBRID');
+Map.centerObject(thicket_boundary);
+// Map.addLayer(masked_image.divide(10000), {min: 0.0, max: [0.3, 0.3, 0.3], bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'S2_SR');
+// Map.addLayer(masked_image, {min: 0.0, max: 3000, bands: ['B4', 'B3', 'B2'], opacity: 1.0}, 'RGB', false);
+Map.addLayer(agc_masked_image, vis, 'AGC');
+Map.layers().get(0).setOpacity(.55)
+/*
+ * Legend setup
+ */
 
+// Creates a color bar thumbnail image for use in legend from the given color
+// palette.
+function make_color_bar_params(palette) 
+{
+  return {
+    bbox: [0, 0, 1, 0.1],
+    dimensions: '100x10',
+    format: 'png',
+    min: 0,
+    max: 1,
+    palette: palette,
+  };
+}
+
+// Create the color bar for the legend.
+var color_bar = ui.Thumbnail({
+  image: ee.Image.pixelLonLat().select(0),
+  params: make_color_bar_params(vis.palette),
+  style: {stretch: 'horizontal', margin: '0px 8px', maxHeight: '24px'},
+});
+
+// Create a panel with three numbers for the legend.
+var legend_labels = ui.Panel({
+  widgets: [
+    ui.Label(vis.min, {margin: '4px 8px'}),
+    ui.Label(
+        (vis.max / 2),
+        {margin: '4px 8px', textAlign: 'center', stretch: 'horizontal'}),
+    ui.Label(vis.max, {margin: '4px 8px'})
+  ],
+  layout: ui.Panel.Layout.flow('horizontal')
+});
+
+var legend_title = ui.Label({
+  value: 'Legend: AGC 2017 (tC/ha)',
+  style: {fontWeight: 'bold'}
+});
+
+var slider = ui.Slider();
+  slider.se
+  slider.setValue(0.6);  // Set a default value.
+  slider.onChange(function(value) {
+    Map.layers().get(0).setOpacity(value);
+  });
+
+var slider_panel = ui.Panel({
+  widgets: [ui.Label('Opacity'), slider],
+  layout: ui.Panel.Layout.flow('horizontal')
+});
+// Add the legendPanel to the map.
+var legend_panel = ui.Panel({widgets: [legend_title, slider_panel, color_bar, legend_labels, 
+                                        ui.Label('More information').setUrl('https://github.com/dugalh/extend_thicket_agc')],   
+                            style: {
+                                position: 'bottom-left',
+                                padding: '4px 8px'
+                              }});
+
+Map.add(legend_panel);
+
+if (false)
+{
+
+  // print(slider);
+  Map.add(slider);
+}
 
