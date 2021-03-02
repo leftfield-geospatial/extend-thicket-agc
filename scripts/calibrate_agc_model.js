31feeaf11bb98e5b22dd87b2d07ef035c1b31b5b
dugalh <dugalh@gmail.com>
2021-03-02 09:51:55 +0000

    Updated scripts/calibrate_agc_model.js (added 2 lines, deleted 2 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -46,8 +46,8 @@ function model_agc(rn_image, train_plots) {
   print('calib_res: ', calib_res);
   
   var calib_coeff = ee.Array(calib_res.get('coefficients')).toList();
-  // print(calib_model.get('coefficients'))
-  //should these be made server side?
+  
+  // should these be made server side?
   var calib_model = {m: ee.Number(ee.List(calib_coeff.get(0)).get(0)), c: ee.Number(ee.List(calib_coeff.get(1)).get(0))};
   var agc_ee_model = {m: calib_model.m.multiply(agc_model.m), c: calib_model.c.multiply(agc_model.m).add(agc_model.c)};
   
