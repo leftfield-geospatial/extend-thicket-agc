d50f860a88724bf15dbc02e9475283922e55b37c
dugalh <dugalh@gmail.com>
2021-03-02 09:24:11 +0000

    Updated scripts/calibrate_agc_model.js (added 4 lines, deleted 5 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -3,6 +3,10 @@ var step_arid_and_valley_thicket = ee.FeatureCollection("users/dugalh/extend_thi
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Calibrate the GEF AGC model to Landsat / Sentinel imagery and evaluate accuracy 
 
+var cloud_masking = require('users/dugalh/extend_thicket_agc:modules/cloud_masking.js');
+var thicket_boundary = step_arid_and_valley_thicket;
+var gef_agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)};
+
 // Find Sentinel or Landsat normalised red image
 function find_rn(image) {
   var rn_image = image.expression('(R / (R + G + B + RE))', 
@@ -16,11 +20,6 @@ function find_rn(image) {
 }
 
 
-var cloud_masking = require('users/dugalh/extend_thicket_agc:modules/cloud_masking.js');
-var thicket_boundary = step_arid_and_valley_thicket;
-
-// the univariate log(mean(R/pan)) WV3 model with pan = (R + G + B + RE) from https://github.com/dugalh/map_thicket_agc
-var gef_agc_model = {m: ee.Number(-318.8304), c: ee.Number(25.7259)};
 // var agc_model = {m: ee.Number(-252.1986), c: ee.Number(16.9453)};  // pan = (R + G + B + NIR1)
 // var agc_model = {m: ee.Number(-245.6729), c: ee.Number(11.5778)};  // pan = (R + G + B + NIR1)
 
