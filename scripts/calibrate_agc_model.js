b5ac29b64f69a313e8159590a7e782725b560fa2
dugalh <dugalh@gmail.com>
2021-03-03 11:10:52 +0000

    Updated scripts/calibrate_agc_model.js (added 1 line, deleted 1 line).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -184,7 +184,7 @@ Export.image.toDrive({
 Export.image.toAsset({
   image: agc_dict.image.uint8(),
   description: 'ee_agc_image_toAsset',
-  folder: 'Earth Engine Data',
+  assetId: 'Earth Engine Data',
   scale: 30,
   region: ee.Feature(thicket_boundary.first()).bounds(),
   fileFormat: 'GeoTIFF',
