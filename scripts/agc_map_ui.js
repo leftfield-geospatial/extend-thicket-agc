50a92f8f95fa9567c42d3e9f737e3f02ff275bca
dugalh <dugalh@gmail.com>
2021-03-03 14:00:49 +0000

    Updated scripts/agc_map_ui.js (added 9 lines, deleted 36 lines).

--- scripts/agc_map_ui.js
+++ scripts/agc_map_ui.js
@@ -1,39 +1,12 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var ee_agc_image = ee.Image("users/dugalh/extend_thicket_agc/ee_agc_image"),
+    step_arid_and_valley_thicket = ee.FeatureCollection("users/dugalh/extend_thicket_agc/step_arid_and_valley_thicket");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Apply the EE model and to EE imagery
 
-var cloud_masking = require('users/dugalh/extend_thicket_agc:modules/cloud_masking.js');
 var thicket_boundary = step_arid_and_valley_thicket;  // STEP derived thicket boundaries
 
-// var s2_toa_images = ee.ImageCollection('COPERNICUS/S2')
-//                   .filterDate('2017-09-01', '2017-11-01')
-//                   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 5))
-//                   .filterBounds(thicket_boundary)
-//                   .map(cloud_masking.s2_simple_cloud_mask);
-
-var l8_sr_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR') //ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')  
-  .filterDate('2017-09-01', '2017-12-30')
-  .filterBounds(thicket_boundary)
-  .map(cloud_masking.landsat8_sr_cloud_mask);
-
-var images = l8_sr_images;
-var image = images.median();    // composite the image collection
-
-// Find R/pan image feature
-function find_rn(image) {
-  var rn_image = image.expression('(R / (R + G + B + RE))',
-    {
-      'R': image.select('B4'),
-      'G': image.select('B3'),
-      'B': image.select('B2'),
-      'RE': image.select(ee.Algorithms.If(image.bandNames().contains('B8'), ['B8'], ['B5']))
-    });
-  return ee.Image(rn_image);
-}
-var rn_image = find_rn(image);
-
-// find the EE AGC image
-var model = { m: ee.Number(ee_agc_model.first().get('m')), c: ee.Number(ee_agc_model.first().get('c')) };
-// print(model);
-var agc_image = (rn_image.log10().multiply(model.m).add(model.c)).uint8();
+var agc_image = ee_agc_image;
 var agc_masked_image = agc_image.clip(thicket_boundary.geometry())
 
 var vis = { min: 0, max: 50, palette: 'red,yellow,green', opacity: 1.0 }
