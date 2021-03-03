6c16dacbf9a3c5179d96558d218a1ef66843c18c
dugalh <dugalh@gmail.com>
2021-03-03 19:09:34 +0000

    Updated scripts/generate_agc_map_ui.js (added 6 lines, deleted 9 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -48,8 +48,11 @@ var mapPanel = ui.Map();
 // Take all tools off the map except the zoom and mapTypeControl tools.
 mapPanel.setControlVisibility({all: false, zoomControl: true, mapTypeControl: true});
 
-// Center the map
+var vis = { min: 0, max: 50, palette: 'red,yellow,green', opacity: 1.0 };
+mapPanel.setOptions('HYBRID');
 mapPanel.centerObject(thicket_boundary);
+mapPanel.addLayer(agc_masked_image, vis, 'AGC');
+mapPanel.layers().get(0).setOpacity(.55)
 
 // Add these to the interface.
 ui.root.widgets().reset([mapPanel]);
@@ -207,12 +210,6 @@ toolPanel.add(viewPanel);
 
 
 // ------------------------------------------------------------------------------------------------------------------------------
-var vis = { min: 0, max: 50, palette: 'red,yellow,green', opacity: 1.0 };
-
-mapPanel.setOptions('HYBRID');
-mapPanel.centerObject(thicket_boundary);
-mapPanel.addLayer(agc_masked_image, vis, 'AGC');
-mapPanel.layers().get(0).setOpacity(.55)
 
 // Creates a color bar thumbnail image for use in legend from the given color
 // palette.
