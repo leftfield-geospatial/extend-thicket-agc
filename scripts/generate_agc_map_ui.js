5d840a907e817021474d504a18bd84419f0dd8f1
dugalh <dugalh@gmail.com>
2021-03-03 19:03:11 +0000

    Updated scripts/generate_agc_map_ui.js (added 7 lines, deleted 6 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -206,14 +206,13 @@ toolPanel.add(viewPanel);
 
 
 
-
 // ------------------------------------------------------------------------------------------------------------------------------
 var vis = { min: 0, max: 50, palette: 'red,yellow,green', opacity: 1.0 };
 
-Map.setOptions('HYBRID');
-Map.centerObject(thicket_boundary);
-Map.addLayer(agc_masked_image, vis, 'AGC');
-Map.layers().get(0).setOpacity(.55)
+// Map.setOptions('HYBRID');
+// Map.centerObject(thicket_boundary);
+// Map.addLayer(agc_masked_image, vis, 'AGC');
+// Map.layers().get(0).setOpacity(.55)
 
 // Creates a color bar thumbnail image for use in legend from the given color
 // palette.
@@ -272,4 +271,6 @@ var legend_panel = ui.Panel({
   }
 });
 
-Map.add(legend_panel);
+// Map.add(legend_panel);
+
+toolPanel.add(legend_panel)
