a84862fb0d56f7893661cae14e12e9acedd91f50
dugalh <dugalh@gmail.com>
2021-03-04 12:16:51 +0000

    Updated scripts/generate_agc_map_ui.js (deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -150,7 +150,6 @@ var legend_labels = ui.Panel({
 //var labelledColorBar = ui.Panel({ widgets: [color_bar, legend_labels],? layout: ui.Panel.Layout.flow('vertical')});
 // var legendWidget = ui.Panel({ widgets: [legendDetail, labelledColorBar], layout: ui.Panel.Layout.flow('horizontal')});
 
-toolPanel.add(labelledColorBar);
 toolPanel.add(legend_labels);
 
 // Define an area for the legend key itself.
