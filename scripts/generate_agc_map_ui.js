70b14a5cd3c97fa6db01019e53d17caa73248be9
dugalh <dugalh@gmail.com>
2021-03-04 14:44:28 +0000

    Updated scripts/generate_agc_map_ui.js (added 39 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -122,5 +122,44 @@ var legendValuesPanel = ui.Panel({
 toolPanel.add(colourBarThumbnail);
 toolPanel.add(legendValuesPanel);
 
+// Chart 
+// Generates a new time series chart of SST for the given coordinates.
+var generateChart = function (coords) {
+  // Update the lon/lat panel with values from the click event.
+  lon.setValue('lon: ' + coords.lon.toFixed(2));
+  lat.setValue('lat: ' + coords.lat.toFixed(2));
+
+  // Add a dot for the point clicked on.
+  var point = ee.Geometry.Point(coords.lon, coords.lat);
+  var dot = ui.Map.Layer(point, {color: '000000'}, 'clicked location');
+  // Add the dot as the second layer, so it shows up on top of the composite.
+  mapPanel.layers().set(1, dot);
+
+  // Make a chart from the time series.
+  var sstChart = ui.Chart.image.series(sst, point, ee.Reducer.mean(), 500);
+
+  // Customize the chart.
+  sstChart.setOptions({
+    title: 'Sea surface temp: time series',
+    vAxis: {title: 'Temp (C)'},
+    hAxis: {title: 'Date', format: 'MM-yy', gridlines: {count: 7}},
+    series: {
+      0: {
+        color: 'blue',
+        lineWidth: 0,
+        pointsVisible: true,
+        pointSize: 2,
+      },
+    },
+    legend: {position: 'right'},
+  });
+  // Add the chart at a fixed position, so that new charts overwrite older ones.
+  inspectorPanel.widgets().set(2, sstChart);
+};
+
+
+
+// 
+
 ui.root.clear();
 ui.root.add(ui.SplitPanel(toolPanel, mapPanel));
