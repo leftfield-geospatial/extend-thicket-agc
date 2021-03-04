27800e89893f4ae1f929a902b04d595c74945a35
dugalh <dugalh@gmail.com>
2021-03-04 15:26:56 +0000

    Updated scripts/generate_agc_map_ui.js (added 3 lines, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -40,7 +40,9 @@ function findAgc(image) {
       'B': image.select('B2'),
       'RE': image.select(ee.Algorithms.If(image.bandNames().contains('B8'), ['B8'], ['B5']))
     });
-  return ee.Image(rnImage.log10().multiply(model.m).add(model.c).set('system:time_start', image.get('system:time_start')));
+  return ee.Image(rnImage.log10().multiply(model.m).add(model.c)
+    .set('system:time_start', image.get('system:time_start')))
+    .rename('AGC');
 }
 
 // var rnImage = findRn(image);
