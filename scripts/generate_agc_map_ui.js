372b036402a38bccaed058009630f1cf0c22224d
dugalh <dugalh@gmail.com>
2021-03-04 13:52:01 +0000

    Updated scripts/generate_agc_map_ui.js (added 2 lines, deleted 2 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -68,8 +68,8 @@ var titleLabel = ui.Label('Thicket Aboveground Carbon (AGC)', {fontWeight: 'bold
 var summaryLabel = ui.Label('Concept demonstration for extension of local AGC model(s) to the thicket biome\n',
     {fontSize: '14px'});
 var detailLabel = ui.Label('An AGC model for a small area in the Baviaanskloof is re-calibrated and applied to ' +
-        'arid and valley thicket areas using Landsat-8 imagery.' + 
-        '\n\nNote that AGC accuracy outside the GEF-5 SLM Baviaanskloof study area has not been established.\n', {fontSize: '11px'});
+        'arid and valley thicket areas using Landsat-8 imagery.')
+var noteLabel = ui.Label('Note that AGC accuracy outside the GEF-5 SLM Baviaanskloof study area has not been established.\n', {fontSize: '11px'});
 var textInfo = ui.Label('See the GitHub repositoty for more information\n\n', {fontSize: '11px'}, 'https://github.com/dugalh/extend_thicket_agc');
 
 var toolPanel = ui.Panel([header, textIntro, textDetail, textInfo], ui.Panel.Layout.Flow('vertical'), {width: '300px'});
