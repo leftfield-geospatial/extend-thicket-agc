99a0451d984b25c2b740a161d6044ea9cc8d2094
dugalh <dugalh@gmail.com>
2021-03-08 14:10:55 +0000

    Updated scripts/generate_agc_map_ui.js (added 1 line, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -15,7 +15,7 @@ var thicketBoundary = stepAridAndValleyThicket;  // STEP derived thicket boundar
 var l8SrImages = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
   .filterBounds(thicketBoundary)
   .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
-  .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 30)
+  .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 35)
   .map(cloudMasking.landsat8_sr_cloud_mask);
 
 var images = l8SrImages.filterDate('2017-09-01', '2017-12-30');
