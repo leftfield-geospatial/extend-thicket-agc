90ec370b60f7a994326c7c3e43e3dd056ef91caf
dugalh <dugalh@gmail.com>
2021-03-08 12:54:19 +0000

    Updated scripts/generate_agc_map_ui.js (added 2 lines, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -150,7 +150,8 @@ var generateChart = function (coords) {
   mapPanel.layers().set(1, dot);
 
   // Make a chart from the time series.
-  var agcChart = ui.Chart.image.series(l8SrImages.filter().map(findAgc), point.buffer(100), ee.Reducer.mean(), 500);
+  var agcChart = ui.Chart.image.series(l8SrImages.filter(ee.Filter.calendarRange(1,1,'month')).map(findAgc), 
+    point.buffer(100), ee.Reducer.mean(), 500);
 
   // Customize the chart.
   agcChart.setOptions({
