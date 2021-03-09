316d07b13349cc21400d282d46a352f96a4af52b
dugalh <dugalh@gmail.com>
2021-03-09 15:27:04 +0000

    Updated scripts/calibrate_agc_model.js (added 8 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -29,6 +29,14 @@ var l8_sr_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
   // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
   .map(cloud_masking.landsat8_sr_cloud_mask);
 
+var l8_toa_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+  .filterDate('2017-09-01', '2017-12-30')
+  .filterBounds(thicket_boundary)
+  // .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
+  // .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 40)
+  // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
+  .map(cloud_masking.landsat8_sr_cloud_mask);
+
 var images = l8_sr_images;
 print('Number of images: ', images.size());
 var image = images.median();    // composite the image collection
