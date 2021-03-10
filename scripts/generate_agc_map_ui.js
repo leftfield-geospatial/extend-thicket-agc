df879015856c584019a955d98f173d6715d6bf63
dugalh <dugalh@gmail.com>
2021-03-10 11:12:50 +0000

    Updated scripts/generate_agc_map_ui.js (added 1 line, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -36,7 +36,7 @@ var model = { m: ee.Number(eeAgcModel.first().get('m')), c: ee.Number(eeAgcModel
 
 // Find R/pan image feature
 function findRn(image) {
-  var rnImage = image.expression('(R / (R + G + B + (RE/3.5)))',
+  var rnImage = image.expression('(R / (R + G + B + (RE/2.5)))',
     {
       'R': image.select('B4'),
       'G': image.select('B3'),
