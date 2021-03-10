b3990341431370b71e398b9daf0080bc0939bb2e
dugalh <dugalh@gmail.com>
2021-03-10 09:52:39 +0000

    Updated scripts/calibrate_agc_model.js (added 8 lines, deleted 8 lines).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -29,15 +29,15 @@ gef_sampling_plots = gef_sampling_plots.map(function (feature) {
 //   // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
 //   .map(cloud_masking.landsat8_sr_cloud_mask);
 
-var l8_toa_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')
-  .filterDate('2017-09-01', '2017-12-30')
-  .filterBounds(thicket_boundary)
-  // .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
-  // .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 40)
-  // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
-  .map(cloud_masking.landsat8_toa_cloud_mask);
+// var l8_toa_images = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')
+//   .filterDate('2017-09-01', '2017-12-30')
+//   .filterBounds(thicket_boundary)
+//   // .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
+//   // .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 40)
+//   // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
+//   .map(cloud_masking.landsat8_toa_cloud_mask);
 
-var images = l8_toa_images;
+var images = l8_sr_images;
 print('Number of images: ', images.size());
 var image = images.median();    // composite the image collection
 
