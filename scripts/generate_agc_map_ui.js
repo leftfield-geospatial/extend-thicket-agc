694c3303460bb0723a8e87e6806407d1a7aac101
dugalh <dugalh@gmail.com>
2021-03-10 13:00:12 +0000

    Updated scripts/generate_agc_map_ui.js (added 1 line, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -16,7 +16,7 @@ var eeAgcModel = eeL8SrAgcModel;
 // Obtain Landsat8 SR image collection of thicket around time of GEF-5 SLM WV3 acquisition
 var l8SrImages = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
   // .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
-  .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 35)
+  .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 40)
   // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
   .map(cloudMasking.landsat8_sr_cloud_mask);
 
