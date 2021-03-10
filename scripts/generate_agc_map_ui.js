e0285d8f8b4fdb2b3718362145a41a6aa950bbc4
dugalh <dugalh@gmail.com>
2021-03-10 14:50:22 +0000

    Updated scripts/generate_agc_map_ui.js (added 1 line, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -176,7 +176,7 @@ var generateChart = function (coords) {
   // var agcChart = ui.Chart.image.series(yearlyMedianImages.map(findAgc), point.buffer(100), ee.Reducer.median(), 30);
   var agcChart = ui.Chart.image.series(images.filter(ee.Filter.calendarRange(9, 12, 'month')).map(findAgc), point.buffer(100), ee.Reducer.median(), 30);
 
-  var sza = images.filter(ee.Filter.calendarRange(9, 12, 'month')).map(function(image){return image.addBands(image.metadata('SOLAR_ZENITH_ANGLE'))});
+  var szaCollection = images.filter(ee.Filter.calendarRange(9, 12, 'month')).map(function(image){return image.addBands(image.metadata('SOLAR_ZENITH_ANGLE'))});
   print(sza);
   
 
