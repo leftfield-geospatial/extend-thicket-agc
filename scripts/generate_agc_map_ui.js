a8dfb794379a4b71f60e1a270aa995fd46ecf1b6
dugalh <dugalh@gmail.com>
2021-03-11 11:07:42 +0000

    Updated scripts/generate_agc_map_ui.js (added 6 lines, deleted 6 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -12,12 +12,12 @@ var s2ToaImages = ee.ImageCollection('COPERNICUS/S2')
                   .filterBounds(thicketBoundary)
                   .map(cloudMasking.s2_simple_cloud_mask);
 
-// // Obtain Landsat8 SR image collection of thicket around time of GEF-5 SLM WV3 acquisition
-// var l8SrImages = ee.ImageCollection('LANDSAT/LC08/C02/T1_SR')
-//   .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
-//   // .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 40)
-//   // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
-//   .map(cloudMasking.landsat8_sr_cloud_mask);
+// Obtain Landsat8 SR image collection of thicket around time of GEF-5 SLM WV3 acquisition
+var l8SrImages = ee.ImageCollection('LANDSAT/LC08/C02/T1_SR')
+  .filterMetadata('GEOMETRIC_RMSE_MODEL', "less_than", 10)
+  // .filterMetadata('SOLAR_ZENITH_ANGLE', "greater_than", 40)
+  // .filterMetadata('SOLAR_AZIMUTH_ANGLE', "less_than", 50)
+  .map(cloudMasking.landsat8_sr_cloud_mask);
 
 // var l8ToaImages = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')
 //   .filterBounds(thicketBoundary)
