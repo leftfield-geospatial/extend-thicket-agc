a1ea1ba63084152b92fc82d89dd6f673894a8656
dugalh <dugalh@gmail.com>
2021-03-15 16:24:50 +0000

    Updated scripts/generate_agc_map_ui.js (added 1 line, deleted 1 line).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -243,7 +243,6 @@ var generateChart = function (coords) {
       legend: { position: "right" },
     });
   
-    // Add the chart at a fixed position, so that new charts overwrite older ones.
     toolPanel.widgets().set(11, szaChart);
   }
 
