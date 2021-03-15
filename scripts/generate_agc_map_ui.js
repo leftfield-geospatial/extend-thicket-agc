c259f7b4ee938c262044ea4c4190113fedc27e31
dugalh <dugalh@gmail.com>
2021-03-15 16:34:32 +0000

    Updated scripts/generate_agc_map_ui.js (added 8 lines, deleted 10 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -191,16 +191,14 @@ function agcTimeSeriesChart(coords) {
   // find [median(images) for images inbetween Sept and Dec in each year]
   var years = ee.List.sequence(2013, 2020);
   var yearlyMedianImages = ee.ImageCollection.fromImages(
-    years
-      .map(function (y) {
-        return images
-          .filter(ee.Filter.calendarRange(y, y, "year"))
-          .filter(ee.Filter.calendarRange(9, 12, "month"))
-          .median()
-          .set("year", y)
-          .set("system:time_start", ee.Date.fromYMD(y, 10, 15));
-      })
-      .flatten()
+    years.map(function (y) {
+      return images
+        .filter(ee.Filter.calendarRange(y, y, "year"))
+        .filter(ee.Filter.calendarRange(9, 12, "month"))
+        .median()
+        .set("year", y)
+        .set("system:time_start", ee.Date.fromYMD(y, 10, 15));
+    }).flatten()
   );
 
   // make a chart of agc(median images)
