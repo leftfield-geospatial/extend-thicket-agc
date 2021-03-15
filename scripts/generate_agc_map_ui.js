07eb6bff136a5b12d027204811479d5185ddc513
dugalh <dugalh@gmail.com>
2021-03-15 16:54:32 +0000

    Updated scripts/generate_agc_map_ui.js (added 3 lines, deleted 3 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -286,13 +286,13 @@ var stepDescrLabel = ui.Label(
   { fontSize: "11px" }
 );
 var stepLinkLabel = ui.Label(
-  "STEP Vegetation Map",  { fontSize: "11px" },
+  "STEP Vegetation Map",  
+  { fontSize: "11px" },
   "https://bgis.sanbi.org/SpatialDataset/Detail/194"
 );
 
 var gefDescrLabel = ui.Label(
-  "AGC model: ",
-  { fontSize: "11px" }
+  "AGC model: ",  { fontSize: "11px" }
 );
 var stepLinkLabel = ui.Label(
   "STEP Vegetation Map",
