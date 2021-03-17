e153fb4419d417d2bed398a415570e573bdebcf3
dugalh <dugalh@gmail.com>
2021-03-17 13:41:56 +0000

    Updated scripts/calibrate_agc_model.js (added 1 line, deleted 1 line).

--- scripts/calibrate_agc_model.js
+++ scripts/calibrate_agc_model.js
@@ -202,7 +202,7 @@ var thicketAgc = agcDict.image.reduceRegion({
   scale: 100,
   maxPixels: 1e8
 });
-print('Total EE AGC (tC/ha): ', thicketAgc);
+print('Total EE AGC (tC/ha): ', thicketArea*agcMean);
 
 // export model and AGC image to EE assets for use in other scripts
 var eeAgcModelFeat = ee.Feature(thicketBoundary.first().geometry().centroid(), agcDict.model);
