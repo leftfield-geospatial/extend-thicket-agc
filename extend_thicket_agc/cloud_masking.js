6930953ed0749e517d49cd6cb46ef365de121e50
Dugal Harris <dugalh@users.noreply.github.com>
2021-03-18 11:52:43 +0200

    add license boilerplate

--- extend_thicket_agc/cloud_masking.js
+++ extend_thicket_agc/cloud_masking.js
@@ -1,4 +1,22 @@
-// Functions for cloud and shadow masking Sentinel-2 and Landsat 8 imagery
+/*
+    Functions for cloud and shadow masking Sentinel-2 and Landsat 8 imagery
+    Copyright (C) 2021 Dugal Harris
+    Email: dugalh@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 exports.defaultCloudThresh = 10
 
 // Simple Sentinel-2 cloud masking using QA60 band
