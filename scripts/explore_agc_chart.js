7b6a839365cd79bb894963bfd91f56341c49725f
dugalh <dugalh@gmail.com>
2023-01-13 13:18:55 +0000

    Updated scripts/explore_agc_chart.js (added 4 lines, deleted 2 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -49,8 +49,10 @@ function findAgc(image) {
     R: image.select("B4"),
     G: image.select("B3"),
     B: image.select("B2"),
-    RE: image.select(
-      ee.Algorithms.If(image.bandNames().contains("B8"), ["B8"], ["B5"])
+    // RE: image.select(
+    //   ee.Algorithms.If(image.bandNames().contains("B8"), ["B8"], ["B5"])
+    // ),
+    RE: image.select("B5"),
     ),
   });
   
