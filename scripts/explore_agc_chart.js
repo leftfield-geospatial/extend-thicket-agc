ec269259956298df0403849930861fc939e06e7c
dugalh <dugalh@gmail.com>
2023-01-14 16:23:21 +0000

    Updated scripts/explore_agc_chart.js (added 8 lines, deleted 7 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -23,7 +23,6 @@ var stepAridAndValleyThicket = ee.FeatureCollection("users/dugalh/extend_thicket
 
 var cloudMasking = require("users/dugalh/extend_thicket_agc:extend_thicket_agc/cloud_masking.js");
 var thicketBoundary = stepAridAndValleyThicket; // STEP derived thicket boundaries
-print(stepAridAndValleyThicket)
 var thicketBounds = stepAridAndValleyThicket.union().bounds();
 
 // obtain Landsat 8 SR image collection of thicket around time of GEF-5 SLM WV3 acquisition
