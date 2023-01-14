128e4bd4d90bec10876685052a463e7506833563
dugalh <dugalh@gmail.com>
2023-01-14 18:31:57 +0000

    Updated scripts/explore_agc_chart.js (added 2 lines, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -68,7 +68,8 @@ function applyScaleFactors(image) {
               .addBands(thermalBands, null, true);
 }
 function findAgc(image) {
-  var rnImage = applyScaleFactors(image).expression("(R / (R + G + B + RE))", {
+  var rnImage = applyScaleFactors(image)
+  rnImage = rnImage.expression("(R / (R + G + B + RE))", {
     R: image.select("SR_B4"),
     G: image.select("SR_B3"),
     B: image.select("SR_B2"),
