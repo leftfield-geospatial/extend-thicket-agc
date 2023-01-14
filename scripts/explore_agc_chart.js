4471564cade46d3fc8010f68c08b58df628bea77
dugalh <dugalh@gmail.com>
2023-01-14 17:30:25 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -29,7 +29,7 @@ var thicketBounds = stepAridAndValleyThicket.union().geometry().bounds();
 var cloudlessColl = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")
   .filterMetadata("GEOMETRIC_RMSE_MODEL", "less_than", 10)
   .filterMetadata("CLOUD_COVER_LAND", "less_than", 50)
-  .filterBounds(thicketBounds)
+  // .filterBounds(thicketBounds)
   .map(cloudMasking.landsat8SrCloudMask);
   
 // var l8SrImages = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")
