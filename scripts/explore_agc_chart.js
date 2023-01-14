4ce729bca6b76832d323a41815c88d9a0ea5eff0
dugalh <dugalh@gmail.com>
2023-01-14 16:08:34 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 8 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -30,13 +30,6 @@ var l8SrImages = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")
   .filterMetadata("GEOMETRIC_RMSE_MODEL", "less_than", 10)
   .filterMetadata("CLOUD_COVER_LAND", "less_than", 50)
   .map(cloudMasking.landsat8SrCloudMask);
-// var l8SrImages = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")
-//   // .filterDate("2017-09-01", "2017-12-30")
-//   .filterDate("2017-01-01", "2017-12-30")
-//   .filterMetadata("CLOUD_COVER_LAND", "less_than", 20)
-//   .filterMetadata("GEOMETRIC_RMSE_MODEL", "less_than", 10)
-//   .filterBounds(thicketBoundary)
-//   .map(cloudMasking.landsat8SrCloudMask);
 
 var eeAgcModel = eeL8SrAgcModel;
 var images = l8SrImages;
