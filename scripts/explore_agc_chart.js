88581c913148decbda57b66c4f975a6ec3ecca04
dugalh <dugalh@gmail.com>
2023-01-14 16:33:44 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 5 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -219,11 +219,7 @@ if (true) // create a time series of yearly AGC
     // find [median(images) for images inbetween Sept and Dec in each year]
     var years = ee.List.sequence(2014, 2022);
     var yearlyMedianImages = ee.ImageCollection.fromImages(
-      years.map(
-        function (year) {
-          return cloudlessComposite(year);
-        }
-      ).flatten()
+      years.map(cloudlessComposite).flatten()
     );
   
     // print(yearlyMedianImages.size());
