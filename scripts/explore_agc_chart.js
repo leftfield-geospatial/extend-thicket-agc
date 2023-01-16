02eca92aaa19ef5741a0f7b51aac3dc9d59f87a4
dugalh <dugalh@gmail.com>
2023-01-16 11:28:08 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -281,7 +281,7 @@ if (true) // create a time series of yearly AGC
       // var equalDate = ee.Filter.equals('system:time_start', xValue);
       // var image = ee.Image(landsat8Toa.filter(equalDate).first());
       var medianLayer = ui.Map.Layer(medianImage, l8Vis);
-      Map.layers().reset([l8Layer, sfLayer]);
+      Map.layers().reset([medianLayer, sfLayer]);
     
       // Show a label with the date on the map.
       label.setValue((new Date(xValue)).toUTCString());
