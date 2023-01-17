2dcc6b3fb8255be0eb963d71a3462f3b50b784ee
dugalh <dugalh@gmail.com>
2023-01-17 16:10:07 +0000

    Updated scripts/explore_agc_chart.js (added 2 lines, deleted 2 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -261,8 +261,8 @@ if (true) // create a time series of yearly AGC
     
     mapPanel.drawingTools().layers().forEach(aggrLayerGeometries);
     print(featColl);
-    geom = featColl.geometry();
-    print(geom);
+    // geom = featColl.geometry();
+    // print(geom);
     // var dot = ui.Map.Layer(geom, { color: "000000" }, "clicked location");
     // mapPanel.layers().set(2, dot);
   
