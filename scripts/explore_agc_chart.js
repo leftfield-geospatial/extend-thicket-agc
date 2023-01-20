f97220817a6fb4d3d90a60cb09bf21ce4f181bbf
dugalh <dugalh@gmail.com>
2023-01-20 12:05:26 +0000

    Updated scripts/explore_agc_chart.js (added 3 lines, deleted 3 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -224,9 +224,9 @@ if (true) // create a time series of yearly AGC
       featColl,
       ee.Reducer.mean(),
       0,
-      30,
-      null,
-      "name"
+      30
+      // null,
+      // "name"
     );
   
     agcChart.setOptions({
