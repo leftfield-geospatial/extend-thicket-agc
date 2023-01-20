aae5326385763accc94d4e8fa56d636d144d57fe
dugalh <dugalh@gmail.com>
2023-01-20 11:33:40 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -327,7 +327,7 @@ if (true) // create a time series of yearly AGC
     };
     stratumGeom.evaluate(addInitGeomLayer);
   };
-  ["Pristine", "Moderate", "Severe"].forEach(addInitGeomLayers);
+  // ["Pristine", "Moderate", "Severe"].forEach(addInitGeomLayers);
   var strataDict = {Pristine: "green", Moderate: "yellow", Severe: "red"};
   for (var stratumName in strataDict){
     addInitGeomLayer(srtatumName, strataDict[srtatumName]);
