41261d1efaa54c6fcf2f10d0864ef3373b78c8be
dugalh <dugalh@gmail.com>
2023-01-21 13:28:35 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -273,7 +273,7 @@ function createAgcChart(mapPanel, toolPanel) {
 
   // make a mean AGC time series chart for geometries
   var agcChart = ui.Chart.image.seriesByRegion(
-    compColl.map(findAgc),
+    compColl.map(findAgc),  // TODO have an AGC collection up front rather than recreate
     layerFeats,
     ee.Reducer.mean(),
     0,
