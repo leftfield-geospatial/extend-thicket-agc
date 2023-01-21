94da11c08e8f15c6a3e8c5719c69256f8231a244
dugalh <dugalh@gmail.com>
2023-01-21 18:24:00 +0000

    Updated scripts/explore_agc_chart.js (added 2 lines, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -31,6 +31,7 @@ var model = {
   m: ee.Number(eeAgcModel.first().get("m")),
   c: ee.Number(eeAgcModel.first().get("c")),
 };
+print(model);
 
 function createComposite(year){
   // Return a yearly median composite of srcColl
