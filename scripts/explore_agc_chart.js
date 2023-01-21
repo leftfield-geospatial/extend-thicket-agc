4e0e302a28935144638bbfafa5dc81f2cbb57764
dugalh <dugalh@gmail.com>
2023-01-21 12:53:36 +0000

    Updated scripts/explore_agc_chart.js (added 2 lines, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -42,7 +42,8 @@ function createComposite(year){
 }
 
 if (true){
-  // Landsat 8 cloud masked collection
+  // Landsat 8
+  // cloud masked collection
   var srcColl = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
     .filterMetadata("GEOMETRIC_RMSE_MODEL", "less_than", 10)
     .filterMetadata("CLOUD_COVER_LAND", "less_than",  20)
