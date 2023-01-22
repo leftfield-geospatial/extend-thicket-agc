fad4c37a85a89e11943f763d767e8ed74ebfa3ef
dugalh <dugalh@gmail.com>
2023-01-22 17:05:38 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -406,7 +406,7 @@ function createAgcChart(mapPanel, toolPanel) {
   agcChart.setOptions({
     title: "AGC: time series",
     vAxis: { title: "AGC (tC/ha)" },
-    hAxis: { title: "Year", format: "yy-qq" },
+    hAxis: { title: "Year", format: "yy-mm" },
     colors: layerColors,
     legend: { position: "right" },
   });
