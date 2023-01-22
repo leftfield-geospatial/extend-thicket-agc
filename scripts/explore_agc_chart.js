0bffd2b6669733cd815f3dfffb12cb06d8af15bf
dugalh <dugalh@gmail.com>
2023-01-22 07:54:29 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -80,7 +80,7 @@ if (true){
     min: 7500,
     max: 13000,
     gamma: 1.2,
-    bands: rgbnbands[:2],
+    bands: rgbnbands.slice(1, 3),
     opacity: 1.0,
   };
   var eeAgcModel = eeL8SrAgcModel;
