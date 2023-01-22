b0d5a3b1b40cc3ba462d1171f2c99bebbc772d8b
dugalh <dugalh@gmail.com>
2023-01-22 08:00:43 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -54,7 +54,7 @@ function annualMedoidComposite(year){
   .filter(ee.Filter.calendarRange(1, 12, "month"))
   .map(medDiff)
   .reduce(ee.Reducer.min(4))
-  .select([0,1,2,3], rgbnBands)
+  .select([0, 1, 2, 3], rgbnBands)
   .set("year", year)
   .set("system:time_start", ee.Date.fromYMD(year, 7, 1));
 
