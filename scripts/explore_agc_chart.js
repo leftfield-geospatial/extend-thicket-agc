84881cdfd3f4065771857e00591ba616a372efa0
dugalh <dugalh@gmail.com>
2023-01-23 13:56:22 +0000

    Updated scripts/explore_agc_chart.js (added 3 lines, deleted 3 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -39,8 +39,8 @@ function medianComp(startDate, months, coll){
   // Return a median composite of coll
   if (!coll) coll = srcColl;
   if (!months) months = compMonths;
-  var stopDate = startDate.advance(months, "month");
-  var compDate = startDate.advance(ee.Number(months).divide(2), "month");
+  var stopDate = ee.Date(startDate).advance(months, "month");
+  var compDate = ee.Date(startDate).advance(ee.Number(months).divide(2), "month");
   return coll.filter(ee.Filter.date(startDate, stopDate))
   .median()
   .set("system:time_start", compDate);
@@ -56,7 +56,7 @@ function medoidComp(startDate, months, coll){
   var compDate = startDate.advance(ee.Number(months).divide(2), "month");
   var medDiff = function(image) {
     // Return the sum of squared differences between image bands and collection median
-    var diff = ee.Image(image).subtract(medianComp).pow(ee.Image.constant(2)); 
+    var diff = ee.Image(image).subtract(_medianComp).pow(ee.Image.constant(2)); 
     return diff.reduce('sum').addBands(image);  
   };
   
