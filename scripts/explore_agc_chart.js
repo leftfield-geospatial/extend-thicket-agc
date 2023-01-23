4a3ce8634f6d29c42e61b97c72e095219637fa49
dugalh <dugalh@gmail.com>
2023-01-23 13:02:19 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 2 lines).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -95,8 +95,7 @@ function qtrMedoidComp(year, quarter, coll){
   };
   
   // find the medoid (pixel from image with smallest distance to collection median)
-  var medoidComp = coll
-  .filter(ee.Filter.calendarRange(year, year, "year"))
+  return coll.filter(ee.Filter.calendarRange(year, year, "year"))
   .filter(ee.Filter.calendarRange((quarter-1)*3+1, (quarter)*3, "month"))
   .map(medDiff)
   .reduce(ee.Reducer.min(5))
