16f642c3499edf03e7f83cdbd07543ea6736811d
dugalh <dugalh@gmail.com>
2023-01-24 15:56:31 +0000

    Updated scripts/explore_agc_chart.js (added 1 line, deleted 1 line).

--- scripts/explore_agc_chart.js
+++ scripts/explore_agc_chart.js
@@ -514,7 +514,7 @@ function createViChart(mapPanel, toolPanel) {
 
   // make a mean AGC time series chart for geometries
   var viChart = ui.Chart.image.seriesByRegion(
-    compColl.map(findAgc),  // TODO have an AGC collection up front rather than recreate
+    compColl.map(findNdvi),  // TODO have an AGC collection up front rather than recreate
     layerFeats,
     ee.Reducer.mean(),
     0,
