dcaf6586f872594fea08d29da8edf53e44711b76
dugalh <dugalh@gmail.com>
2024-08-21 09:24:46 +0000

    Updated scripts/generate_agc_map_ui.js (added 4 lines, deleted 109 lines).

--- scripts/generate_agc_map_ui.js
+++ scripts/generate_agc_map_ui.js
@@ -48,9 +48,7 @@ function findAgc(image) {
     R: image.select("SR_B4"),
     G: image.select("SR_B3"),
     B: image.select("SR_B2"),
-    RE: image.select(
-      ee.Algorithms.If(image.bandNames().contains("SR_B8"), ["SR_B8"], ["SR_B5"])
-    ),
+    RE: image.select("SR_B5"),
   });
   
   return ee.Image(rnImage.log10()
@@ -83,7 +81,7 @@ var vis = {
 mapPanel.addLayer(agcMaskedImage, vis, "AGC");
 
 // Create side tool panel
-  // title and description
+// title and description
 var titleLabel = ui.Label("Thicket Aboveground Carbon (AGC)", {
   fontWeight: "bold",
   fontSize: "24px",
@@ -113,7 +111,7 @@ var toolPanel = ui.Panel({
   style: { width: "20%" },
 });
 
-  // legend and its controls
+// legend and its controls
 var legendTitleLabel = ui.Label("Legend", {
   fontWeight: "bold",
   fontSize: "20px",
@@ -172,7 +170,7 @@ var colourBarThumbnail = ui.Thumbnail({
   style: { stretch: "horizontal", margin: "0px 8px", maxHeight: "24px" },
 });
 
-  // value labels for colour bar
+// value labels for colour bar
 var legendValuesPanel = ui.Panel({
   widgets: [
     ui.Label(vis.min, { margin: "4px 8px" }),
@@ -189,108 +187,6 @@ var legendValuesPanel = ui.Panel({
 toolPanel.add(colourBarThumbnail);
 toolPanel.add(legendValuesPanel);
 
-if (false) // create a time series of yearly AGC
-{
-  var agcTimeSeriesChart = function(coords) {
-    // show the clicket point
-    var point = ee.Geometry.Point(coords.lon, coords.lat);
-    var dot = ui.Map.Layer(point, { color: "000000" }, "clicked location");
-    mapPanel.layers().set(1, dot);
-  
-    // find [median(images) for images inbetween Sept and Dec in each year]
-    var years = ee.List.sequence(2013, 2020);
-    var yearlyMedianImages = ee.ImageCollection.fromImages(years.map(function (y) {
-        return images
-          .filter(ee.Filter.calendarRange(y, y, "year"))
-          .filter(ee.Filter.calendarRange(9, 12, "month"))
-          .median()
-          .set("year", y)
-          .set("system:time_start", ee.Date.fromYMD(y, 10, 15));
-      }).flatten()
-    );
-  
-    // make a chart of agc(median images)
-    var agcChart = ui.Chart.image.series(
-      yearlyMedianImages.map(findAgc),
-      point.buffer(100),
-      ee.Reducer.median(),
-      30
-    );
-  
-    agcChart.setOptions({
-      title: "AGC: time series",
-      vAxis: { title: "AGC (tC/ha)" },
-      hAxis: { title: "Date", format: "MM-yy", gridlines: { count: 7 } },
-      series: {
-        0: {
-          color: "SteelBlue",
-          lineWidth: 1,
-          pointsVisible: true,
-          pointSize: 3,
-        },
-      },
-      legend: { position: "right" },
-    });
-    
-    toolPanel.widgets().set(10, agcChart);
-  
-    if (false)    // create a chart of solar zenith and azimuth angle for debugging agc
-    {
-      var szaCollection = images
-        .filter(ee.Filter.calendarRange(9, 12, "month"))
-        .map(function (image) {
-          return image.addBands([
-            image.metadata("SOLAR_ZENITH_ANGLE"),
-            image.metadata("SOLAR_AZIMUTH_ANGLE"),
-          ]);
-        });
-      
-      var szaChart = ui.Chart.image.series(
-        szaCollection.select(["SOLAR_ZENITH_ANGLE", "SOLAR_AZIMUTH_ANGLE"]),
-        point,
-        ee.Reducer.mean(),
-        30
-      );
-    
-      szaChart.setOptions({
-        title: "SZA: time series",
-        vAxis: { title: "SZA (deg)" },
-        hAxis: { title: "Date", format: "MM-yy", gridlines: { count: 7 } },
-        series: {
-          0: {
-            color: "SteelBlue",
-            lineWidth: 0,
-            pointsVisible: true,
-            pointSize: 3,
-          },
-        },
-        legend: { position: "right" },
-      });
-      toolPanel.widgets().set(11, szaChart);
-    }
-  };
-  mapPanel.onClick(agcTimeSeriesChart);
-  mapPanel.style().set("cursor", "crosshair");
-  
-  // test point for AGC chart
-  // var initialPoint = ee.Geometry.Point(24.37007063238984017, -33.66776731422557845);   //Baviaanskloof Smitskraal
-  var initialPoint = ee.Geometry.Point(23.94436842431511536, -33.55374308591438393); //Baviaanskloof Sewefontein
-  // var initialPoint = ee.Geometry.Point(22.21972695106567031, -33.57070965396300011);  // Oudtshoorn Grootkop
-  // mapPanel.centerObject(initialPoint, 4);
-  
-  var chartTitleLabel = ui.Label("Time Series", {
-    fontWeight: "bold",
-    fontSize: "20px",
-    color: "SteelBlue",
-  });
-  toolPanel.add(chartTitleLabel);
-  
-  agcTimeSeriesChart({
-    lon: initialPoint.coordinates().get(0).getInfo(),
-    lat: initialPoint.coordinates().get(1).getInfo(),
-  });
-}
-
 // make credits panel
 var creditsTitleLabel = ui.Label("Acknowledgements", {
   fontWeight: "bold",
@@ -306,7 +202,6 @@ var stepLinkLabel = ui.Label(
   "STEP vegetation map",  
   { fontSize: "11px", margin: "4px 0px" },
   "https://bgis.sanbi.org/STEP/project.asp"
-  // "https://bgis.sanbi.org/SpatialDataset/Detail/194"
 );
 var stepPanel = ui.Panel(
   [stepDescrLabel, stepLinkLabel],
